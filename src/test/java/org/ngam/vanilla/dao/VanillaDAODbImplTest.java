828405119a89aa9ec1b76ac5a2ac6156d91ce14a
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-09-16 23:00:09 +0200

    Change from using Movie.equals() for equality testing on hand-constructed class (with 0 as id) and retrieved object (with non-zero id), as this makes for an inaccurate test. Instead, test each field using getters while leaving out id.

--- src/test/java/org/ngam/vanilla/dao/VanillaDAODbImplTest.java
+++ src/test/java/org/ngam/vanilla/dao/VanillaDAODbImplTest.java
@@ -6,6 +6,7 @@ import org.ngam.vanilla.entities.Movie;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by phoenix on 9/15/16.
@@ -16,8 +17,17 @@ public class VanillaDAODbImplTest {
     public void testAddMovie() {
         Movie movie = new Movie(0,"Jaws","Steven Spielberg","Story of a great white shark!");
         VanillaDAO_dbImpl dao = new VanillaDAO_dbImpl();
+        // System.out.println("Adding movie : "+movie);
         dao.addMovie(movie);
         List<Movie> movies = dao.getMovies();
-        assertTrue(movies.contains(movie));
+        
+        assertTrue(movies.size() > 0);
+
+        Movie db_mov = movies.get(movies.length()-1);
+        // System.out.println("Got movie : "+db_mov);
+        assertEquals(movie.getTitle(),db_mov.getTitle());
+        assertEquals(movie.getDirector(),db_mov.getDirector());
+        assertEquals(movie.getSynopsis(),db_mov.getSynopsis());
+        //assertTrue(movies.contains(movie)); // equals won't work here because id's aren't the same!
     }
 }
